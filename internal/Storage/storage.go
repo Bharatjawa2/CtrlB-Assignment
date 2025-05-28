b14440e7855098dbbf37f90acabec508a21cd42f
Bharat <jawabharat05@gmail.com>
2025-05-28 11:33:55 +0530

    Storage changes

--- internal/Storage/storage.go
+++ internal/Storage/storage.go
@@ -3,6 +3,6 @@ package storage
 import "github/Bharatjawa2/CtrlB_Assignment/models"
 
 type Storage interface{
-	CreateStudent(name string,email string, age int)(int64,error)
+	CreateStudent(name string,email string,password string,age int,gender string,phoneNumber string,DOB string,Address string)(int64,error)
 	GetStudentById(id int64) (models.Student,error)
 }
